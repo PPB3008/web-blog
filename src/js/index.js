ae4a0e2c4e244f78a98c9314675cf528a5aa7ea2
PPB3008 <deccsec@gmail.com>
2017-11-19 05:42:12 +0800

    更改大量主页版面样式
    Signed-off-by: PPB3008 <deccsec@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,13 @@
 
 
   let blogObj=[
-     {"ID":"31001","title":"firstinject","content":"1233","time":"2017-08-11 16:16"},
-     {"ID":"31002","title":"firstinject","content":"1234","time":"2017-08-11 16:16"},
-     {"ID":"31003","title":"firstinject","content":"1235","time":"2017-08-11 16:16"},
-     {"ID":"31004","title":"firstinject","content":"1236","time":"2017-08-11 16:16"},
-     {"ID":"31005","title":"firstinject","content":"1237","time":"2017-08-11 16:16"},
-     {"ID":"31006","title":"firstinject","content":"1238","time":"2017-08-11 16:16"},
-     {"ID":"31007","title":"firstinject","content":"1239","time":"2017-11-17 04:51"}
+     {"ID":"31001","title":"firstinject","content":"1233","time":"2017-08-11 "},
+     {"ID":"31002","title":"firstinject","content":"1234","time":"2017-08-11 "},
+     {"ID":"31003","title":"firstinject","content":"1235","time":"2017-08-11 "},
+     {"ID":"31004","title":"firstinject","content":"1236","time":"2017-08-11 "},
+     {"ID":"31005","title":"firstinject","content":"1237","time":"2017-08-09 "},
+     {"ID":"31006","title":"firstinject","content":"1238","time":"2017-08-12 "},
+     {"ID":"31007","title":"firstinject","content":"1239","time":"2017-11-17 "}
   ];
 
 
@@ -29,23 +29,22 @@
         <div class="blog-item"> 
         <div class="blog-item-content">
         
-        <div class="control">
-          <span>${blogObj[i].ID}</span>
+         <h2>${blogObj[i].title}</h2>
+         
+         <div class="article-content">${blogObj[i].content}
+         </div>
+             
+         <div class="control">
+          <span>${blogObj[i].time}</span>
           <input type="button">
          </div>
-        
-         <h2>${blogObj[i].title}</h2>
-         <div>${blogObj[i].content}</div>
-            
+         
          <div class="item-light">
-           <div class="item-user"></div>    //点赞用户
-           <div>
                <span>Yes</span>
                <span>/</span>
                <span>No</span>
-           </div>
-        </div>
-        
+          </div>
+
         </div>
         </div>
         `;     
