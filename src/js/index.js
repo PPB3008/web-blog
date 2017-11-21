b22f6820a4483bc6e99c98fdecc91083f786c40e
PPB3008 <deccsec@gmail.com>
2017-11-21 12:17:19 +0800

    更改部分样式

--- src/js/index.js
+++ src/js/index.js
@@ -7,10 +7,12 @@
      {"ID":"31004","title":"firstinject","content":"1236","time":"2017-08-11 "},
      {"ID":"31005","title":"firstinject","content":"1237","time":"2017-08-09 "},
      {"ID":"31006","title":"firstinject","content":"1238","time":"2017-08-12 "},
-     {"ID":"31007","title":"firstinject","content":"1239","time":"2017-11-17 "}
+     {"ID":"31007","title":"firstinject","content":"1235","time":"2017-11-17 "}
   ];
 
-
+function myaddEvent(element,event,listener){
+   element["on"+event]=listener;
+}
 
 
 //alert(Date());
@@ -22,16 +24,26 @@
         for(i in this.content){
            blogContent[0].innerHTML+=this.content[i];
          }     
+         this.addClick();
         },
+      addClick:function(){
+        let blogItem=document.getElementsByClassName("blog-item");
+        for(i in blogItem){
+          myaddEvent(blogItem[i],"click",function(){
+            window.open("../src/blog.html",true)
+          });
+        }
+      },
       textClear:function (){
         for(let i in blogObj){
         this.content[i]=`
-        <div class="blog-item"> 
+        <div class="blog-item" > 
         <div class="blog-item-content">
         
          <h2>${blogObj[i].title}</h2>
          
-         <div class="article-content">${blogObj[i].content}
+         <div class="article-content">
+         ${blogObj[i].content}
          </div>
              
          <div class="control">
@@ -49,6 +61,7 @@
         </div>
         `;     
    }
+   this.addArticle();
   }
 }
 
@@ -58,7 +71,6 @@
 
 window.onload=function(){
   ArticleOP.textClear();
-  ArticleOP.addArticle();
 }
 
 
